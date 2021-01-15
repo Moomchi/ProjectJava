6e52b6fc778a014b6326ecabc8fa0ec5ecee3ca5
Imortalco <vanko.krastev@gmail.com>
2021-01-15 18:31:29 +0200

    Работещ saveProduct метод + малко работа по frontenda

---
+++ FoodDelivery-backend/src/main/java/com/mimi/FoodDelivery/beans/ProductRequest.java
@@ -0,0 +1,49 @@
+package com.mimi.FoodDelivery.beans;
+
+public class ProductRequest {
+    Long id;
+    Integer customerId;
+    Integer num;
+    Integer productId;
+    Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
